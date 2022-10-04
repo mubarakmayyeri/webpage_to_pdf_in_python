5f3e1c80570006f903cfa18d567d01b98a213308
Mubarak Mayyeri <mubarakmayyeri@gmail.com>
2022-10-05 01:02:07 +0530

    minor updates

--- webpage_converter.py
+++ webpage_converter.py
@@ -6,4 +6,6 @@ in_url = input("Paste url here : ")
 url_split = in_url.split(".")
 web_name = url_split[1]
 
-pdfkit.from_url(in_url,'.\webpage_to_pdf\\Output\\' + web_name +'.pdf', configuration = config) 
+pdfkit.from_url(in_url,'Output\\' + web_name +'.pdf', configuration = config) 
+
+print("\nYour pdf file will be stored in Output folder.\n")
